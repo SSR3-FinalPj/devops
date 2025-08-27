9256cb86484e071ef8e38e3fa68b80ba7ec65132
Oldentomato <ender1412@naver.com>
2025-08-27 10:43:02 +0900

    secret 변경

---
+++ schedulerServer/deleteHelm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash  
+
+kubectl delete deployment airflow-service-scheduler -n dev-system
+kubectl delete deployment airflow-service-webserver -n dev-system
+kubectl delete deployment token-updater -n dev-system
+kubectl delete pod airflow-worker-pod-template -n dev-system
+kubectl delete serviceaccount airflow-service-sa -n dev-system
+kubectl delete role airflow-service-role -n dev-system
+kubectl delete rolebinding airflow-service-rolebinding -n dev-system
+kubectl delete service scheduler-service -n dev-system
+kubectl delete configmap airflow-service-config -n dev-system
+kubectl delete job airflow-service-init -n dev-system
+kubectl delete sealedsecret airflow-service-secrets -n dev-system
+
+
